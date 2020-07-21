487dea048ec0db16242146c170890a296f47625f
Usuario <Usuario@DESKTOP-0C6FICR>
2020-07-21 16:11:14 -0500

    first commit of the base app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class HelloWorld extends React.Component {
+
+state = {
+  show: true
+}
+
+toggleShow = () => {
+  this.setState({show: !this.state.show});
+}
+
+
+  render() {
+    if(this.state.show){
+      return (
+        <div id="Hello">
+        <h3>{this.props.language}</h3>
+        {this.props.mytext}
+        <button onClick={this.toggleShow}>Toggle Show</button>
+        </div>
+        )
+    } else {
+      return <h1>THERE ARE NO ELEMENTS!
+      <button onClick={this.toggleShow}>
+        Toggle Show
+      </button>
+      </h1>
+    }
+
+ 
+  }
+}
+
+// const App = () => <div>This is my component: <HelloWorld></HelloWorld> </div>
+
+// class App extends React.Component {
+//   render() {
+//     return <div>This is my component <HelloWorld></HelloWorld></div>
+//   }
+// }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>This is my component: 
+     <HelloWorld mytext="Hello Dacta" language="english"></HelloWorld>
+      <HelloWorld mytext="Konichiwa Dacta">
+       </HelloWorld> <HelloWorld mytext="Oui Dacta"></HelloWorld>  
+       </div>
   );
 }
 
